29a1dfab3815fe36c4211287f49058e68cad6121
Nate Worley <immythz@gmail.com>
2021-06-07 17:30:44 -0500

    wrote user prompts and criteria logic

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,5 @@
+const symbols = '!@#$%^&*()=<>,./[]{}';
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -8,6 +10,41 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+
+// User prompts
+function passwordLength() {
+  const passwordPrompt = prompt('How many characters would you like you password to be? (8-128)');
+  if (passwordPrompt < 8) {
+    alert("Password length must be between 8 and 128 characters");
+    return;
+  }
+}
+const passwordPrompt = prompt('How many characters would you like you password to be? (8-128)')
+const lowercasePrompt = confirm("Would you like to include lowercase letters?");
+const uppercasePrompt = confirm("Would you like to include lowercase letters?");
+const numberPrompt = confirm("Would you like to include lowercase letters?");
+const symbolPrompt = confirm("Would you like to include lowercase letters?");
+
+
+// Lowercase function
+function randomLowercase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+// Uppercase function
+function randomUppercase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+// Numeric function
+function randomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+// and/or special characters function
+function randomSymbol() {
+  return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
 // Add event listener to generate button
